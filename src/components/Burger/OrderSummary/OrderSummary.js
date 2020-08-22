8c954300df7cd64300d458743dcc161b9853932b
Asutosh Rout <asutoshrt96@gmail.com>
2020-08-23 01:37:37 +0530

    3rd

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props)  => {
+
+    const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+        return <li key={igKey}>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+    })
+
+    return(
+        <Aux>
+            <h3>Your Order-></h3>
+            <p>Ye sab hain isme -- >></p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout???</p>
+            <button>Cancel</button>
+            <button>Contine!!</button>
+        </Aux>
+    )
+};
+
+export default orderSummary
